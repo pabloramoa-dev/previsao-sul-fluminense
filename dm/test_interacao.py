78a7f96200f1349d1142d8e6f651641e94352f82
pabloramoa-dev <pablo.ramoa@gmail.com>
2026-08-25 19:48:59 -0300

    Expandir cobertura para todo o Sul Fluminense (#13)
    * feat: expandir cobertura para 14 cidades
    
    * feat: adicionar coordenadas das novas cidades
    
    * test: validar bairros e localidades das novas cidades
    
    * fix: preservar bairro quando a cidade vem na mensagem
    
    * fix: limpar separador do formato bairro e cidade

--- dm/test_interacao.py
+++ dm/test_interacao.py
@@ -5,6 +5,7 @@ import unittest
 os.environ.pop("DATABASE_URL", None)
 
 from interacao import comando, relato
+from dm_bairro import resolver
 import radar
 
 
@@ -20,6 +21,16 @@ class InteracaoTest(unittest.TestCase):
     def test_relato_com_cidade(self):
         situacao, cidade, _, condicao = relato("sol no Centro de Resende")
         self.assertEqual((situacao, cidade, condicao), ("cidade", "Resende", "sol"))
+
+    def test_bairro_novo_com_cidade(self):
+        situacao, cidade, rotulo = resolver("Parque Mambucaba, Angra dos Reis")
+        self.assertEqual((situacao, cidade), ("cidade", "Angra dos Reis"))
+        self.assertIn("Parque Mambucaba", rotulo)
+
+    def test_localidade_livre_com_cidade(self):
+        situacao, cidade, rotulo = resolver("Minha Localidade, Paraty")
+        self.assertEqual((situacao, cidade), ("cidade", "Paraty"))
+        self.assertEqual(rotulo, "Minha Localidade (Paraty)")
 
     def test_deduplicacao(self):
         self.assertTrue(radar.registrar("mid-teste", "u1", "Resende", "Centro", "sol"))
